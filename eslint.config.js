18de60765b1b6371690df092b2edab5510005f12
Anastasiya Bogdanova <nastena40480@yandex.ru>
2026-07-28 17:24:46 +0300

    edit Ci

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,24 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import { defineConfig } from 'eslint/config'
-import stylistic from '@stylistic/eslint-plugin'
+import js from '@eslint/js';
+import globals from 'globals';
 
-export default defineConfig([
-  stylistic.configs.recommended,
+export default [
+  js.configs.recommended,
   {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.node },
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+      'import/extensions': 'off',
+      'import/no-unresolved': 'off',
+      'no-underscore-dangle': 'off',
+      'linebreak-style': 'off',
+      'eol-last': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
   },
-])
+];
