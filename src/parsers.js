94b6db50e2d7bcefdc19bd5fa0c530dddb00b9c5
Anastasiya Bogdanova <nastena40480@yandex.ru>
2026-08-03 14:47:49 +0300

    add switch case

--- src/parsers.js
+++ src/parsers.js
@@ -7,13 +7,13 @@ export default function parseFile(filepath) {
   const content = fs.readFileSync(fullPath, 'utf-8');
   const extension = path.extname(filepath).toLowerCase();
 
-  if (extension === '.json') {
-    return JSON.parse(content);
+  switch (extension) {
+    case '.json':
+      return JSON.parse(content);
+    case '.yml':
+    case '.yaml':
+      return yaml.load(content);
+    default:
+      throw new Error(`Unsupported file format: ${extension}`);
   }
-
-  if (extension === '.yml' || extension === '.yaml') {
-    return yaml.load(content);
-  }
-
-  throw new Error(`Unsupported file format: ${extension}`);
 }
